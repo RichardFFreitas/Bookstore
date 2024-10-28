1103859d82090347569d88f38790245d235797c5
Richard <richardfelipelives@gmail.com>
2024-10-28 20:21:55 -0300

    Make-tests and serializers

--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import api_home
-
-urlpatterns = [
-    path('api/', api_home),
-]
--- api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-@api_view(['GET'])
-def api_home(request):
-    return Response({"message": "Hello, Django REST Framework!"})
-
