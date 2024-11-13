0439a2a4b31e840c15619165aa48b7d1450d9dcc
Richard <richardfelipelives@gmail.com>
2024-11-13 12:20:25 -0300

    adicionando link do python anywhere

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-_uql)*)q-k=yx15(*vw#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'richardffreitas.pythonanywhere.com']
 
 # Application definition
 
